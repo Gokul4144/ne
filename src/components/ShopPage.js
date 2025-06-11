fe1d4baa48e2d6a2003f3a1de1aad76f089dd315
Gokul4144 <71762307019@cit.edu.in>
2025-06-11 06:13:02 +0530

    Update ShopPage.js

--- src/components/ShopPage.js
+++ src/components/ShopPage.js
@@ -1,9 +1,12 @@
+import React from 'react';
 import LivePlayers from './LivePlayers';
+import DiscountTimer from './DiscountTimer';
 
 function ShopPage() {
   return (
     <div>
       <h1>Welcome to the Shop</h1>
+      <DiscountTimer />
       <LivePlayers />
     </div>
   );
