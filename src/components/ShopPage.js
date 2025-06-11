aae739f1089664ea6ac5f6f2477b844f5f438991
Gokul4144 <71762307019@cit.edu.in>
2025-06-11 06:10:37 +0530

    Add files via upload

---
+++ src/components/ShopPage.js
@@ -0,0 +1,12 @@
+import LivePlayers from './LivePlayers';
+
+function ShopPage() {
+  return (
+    <div>
+      <h1>Welcome to the Shop</h1>
+      <LivePlayers />
+    </div>
+  );
+}
+
+export default ShopPage;
